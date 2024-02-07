1db857339b2a95f9e51852e1fd8073921965803d
TatianaShes <lampone66@mail.ru>
2024-02-07 18:46:02 +0500

    Добовляет адаптива логотипу

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,10 @@
 const menu = document.querySelector('.nav');
 const menuBtn = document.querySelector('.nav__toggle');
 
+if (document.querySelector('.nojs')) {
+  document.querySelector('.nojs').classList.remove('nojs');
+}
+
 menuBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
   menu.classList.toggle('is-open');
